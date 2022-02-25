2eab557ed9c76c81ec92fb95f3d8f89efb314d32
Roshni Vachhani <Roshni.Vachhani@tts.edu.sg>
2022-02-25 01:53:05 +0000

    Fix the loop

--- csmodules.php
+++ csmodules.php
@@ -1,13 +1,13 @@
 <form method="post" action="output.php">      
     <fieldset>      
         <legend>Which of these modules did you like best? (please select up to 2)</legend>      
-        <input type="checkbox" name="fav_mod" value="COMP1021">Mathematics for Computer Science<br>      
-        <input type="checkbox" name="fav_mod" value="COMP1051">Computational Thinking<br>      
-        <input type="checkbox" name="fav_mod" value="COMP1071">Computer Systems<br>      
-        <input type="checkbox" name="fav_mod" value="COMP1081">Algorithms and Data Structures<br>     
-        <input type="checkbox" name="fav_mod" value="COMP1101">Programming (Black)<br>     
-        <input type="checkbox" name="fav_mod" value="COMP1111">Programming (Gold)<br>          
+        <input type="checkbox" name="csfav_mod1" value="COMP1021">Mathematics for Computer Science<br>      
+        <input type="checkbox" name="csfav_mod2" value="COMP1051">Computational Thinking<br>      
+        <input type="checkbox" name="csfav_mod3" value="COMP1071">Computer Systems<br>      
+        <input type="checkbox" name="csfav_mod4" value="COMP1081">Algorithms and Data Structures<br>     
+        <input type="checkbox" name="csfav_mod5" value="COMP1101">Programming (Black)<br>     
+        <input type="checkbox" name="csfav_mod6" value="COMP1111">Programming (Gold)<br>          
         <br>      
-        <input type="submit" value="Submit now" name="submit />      
+        <input type="submit" value="Submit now" />      
     </fieldset>      
 </form>
--- output.php
+++ output.php
@@ -1,15 +1,41 @@
 <?php
-
-include_once 'config.php';
-if(isset($_POST['submit'])){
-
-    if(!empty($_POST['fav_mod'])) {
-
-        foreach($_POST['fav_mod'] as $value){
-            echo "value : ".$value.'<br/>';
-        }
-
+    if(isset($_POST['pfav_mod1'])){
+        echo "PSYC1061"."<br>";
     }
-
-}
+    if(isset($_POST['pfav_mod2'])){
+        echo "PSYC1062"."<br>";
+    }
+    if(isset($_POST['pfav_mod3'])){
+        echo "PSYC1071"."<br>";
+    }
+    if(isset($_POST['pfav_mod4'])){
+        echo "PSYC1081"."<br>";
+    }
+    if(isset($_POST['pfav_mod5'])){
+        echo "PSYC1101"."<br>";
+    }
+    if(isset($_POST['pfav_mod6'])){
+        echo "PSYC1111"."<br>";
+    }
+    if(isset($_POST['pfav_mod7'])){
+        echo "PSYC1121"."<br>";
+    }
+    if(isset($_POST['csfav_mod1'])){
+        echo "COMP1021"."<br>";
+    }
+    if(isset($_POST['csfav_mod2'])){
+        echo "COMP1051"."<br>";
+    }
+    if(isset($_POST['csfav_mod3'])){
+        echo "COMP1071"."<br>";
+    }
+    if(isset($_POST['csfav_mod4'])){
+        echo "COMP1081"."<br>";
+    }
+    if(isset($_POST['csfav_mod5'])){
+        echo "COMP1101"."<br>";
+    }
+    if(isset($_POST['csfav_mod6'])){
+        echo "COMP1111"."<br>";
+    }
 ?>
