de21185b33e3afa5726070fb8d6cb07abef0e8a6
Roshni Vachhani <Roshni.Vachhani@gmail.com>
2022-05-28 19:55:06 +0100

    Create choicerec.php
    choose between user suggestion and item suggestion

---
+++ choicerec.php
@@ -0,0 +1,14 @@
+<?php
+
+
+?>
+
+<form action="similaruserrec.php" method="post">
+Similar users got
+<input type='submit' name='submit' value='yes'  />
+</form>
+
+<form action="user.php" method="post">
+your profile
+<input type='submit' name='submit' value='yes'  />
+</form>
