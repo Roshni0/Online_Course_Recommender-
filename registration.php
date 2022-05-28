bf49119f5ed527675940a91376f1c15851ed9a2e
Roshni Vachhani <Roshni.Vachhani@gmail.com>
2022-05-28 19:57:09 +0100

    added sessions

--- registration.php
+++ registration.php
@@ -14,14 +14,23 @@ if (mysqli_num_rows($result) > 0) {
 } else {
     $sql = "INSERT INTO `user_details` (`userId`, `username`, `gender`, `age`) VALUES (NULL, '$name', '$gender', $age)";
     if (mysqli_query($conn, $sql)) {
+      $sql = "SELECT * FROM `user_details` WHERE `username`='$name'";
+      $result = mysqli_query($conn, $sql);
+      
+      if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+          echo "id: " . $row["userId"]. " - Age: " . $row["age"]. " Gender " . $row["gender"]. "<br>";
+          session_start();
+          $_SESSION['userId'] = $row["userId"];
     echo "New record has been added successfully !";
     header("Location: http://localhost/xai/welcome.php");
     exit();
-    } else {
+    }} else {
     echo "Error: " . $sql . ":-" . mysqli_error($conn);
     }
 }
-
+}
 mysqli_close($conn);
 
 
