ee34d3895aaa11798d25915908f0a64c107cc07d
Roshni Vachhani <Roshni.Vachhani@gmail.com>
2022-05-29 14:52:25 +0100

    idk trying to input data?

---
+++ enter_data.php
@@ -0,0 +1,232 @@
+<?php
+include_once 'config.php';
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1061comms', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1062res', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1062des', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1062rep', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1062issues', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1071brain', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1071vision', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1071atm', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1071learn', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1081dif', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1081dev', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1081soc', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1101', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1111', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1121', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061sci,p1062rep', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1062res', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1062des', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1062rep', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p10621issue', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1071brain', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1071vision', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1071atm', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1071learn', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1081dif', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1081dev', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1081soc', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1101', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1111', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1061comms,p1121', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1062des', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1062rep', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p10621issue', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1071brain', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1071vision', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1071atm', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1071learn', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1081dif', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1081dev', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1081soc', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1101', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1111', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062res,p1121', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p1062rep', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p10621issue', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p1071brain', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p1071vision', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p1071atm', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p1071learn', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p1081dif', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p1081dev', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p1081soc', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p1101', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p1111', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062des,p1121', '')";
+if (mysqli_query($conn, $sql)) {}
+
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062rep,p10621issue', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062rep,p1071brain', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062rep,p1071vision', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062rep,p1071atm', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062rep,p1071learn', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062rep,p1081dif', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062rep,p1081dev', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062rep,p1081soc', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062rep,p1101', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062rep,p1111', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062rep,p1121', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062issue,p1071brain', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062issue,p1071vision', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062issue,p1071atm', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062issue,p1071learn', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062issue,p1081dif', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062issue,p1081dev', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062issue,p1081soc', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062issue,p1101', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062issue,p1111', '')";
+if (mysqli_query($conn, $sql)) {}
+
+$sql ="INSERT INTO `topics` (`user_topic`, `userId`, `modules`, `suggestion`) VALUES (NULL, '1', 'p1062issue,p1121', '')";
+if (mysqli_query($conn, $sql)) {}
+
